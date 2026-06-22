6badb02245682b6273ba008a60af6cd5f4249199
VBs8642 <vanesabombeva2@gmail.com>
2026-06-22 05:29:36 +0300

    feat: registration entity added

---
+++ event-hub/src/main/java/com/event_hub/event_hub/model/entity/user/Registration.java
@@ -0,0 +1,31 @@
+package com.event_hub.event_hub.model.entity.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "registrations")
+public class Registration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "attendee_id")
+    private User attendee;
+
+    private LocalDateTime registrationDate = LocalDateTime.now();
+    private int attendeesCount;
+}
