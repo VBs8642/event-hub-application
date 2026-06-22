87f4c5e8ec850e417a061997f0a8b7ddf3f7f711
VBs8642 <vanesabombeva2@gmail.com>
2026-06-23 02:42:08 +0300

    feat: created EventUpdateDto,RegistartionDto, UserDto

---
+++ event-hub/src/main/java/com/event_hub/event_hub/model/dto/user/UserDto.java
@@ -0,0 +1,20 @@
+package com.event_hub.event_hub.model.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+public class UserDto {
+
+    private UUID id;
+    private String username;
+    private String email;
+    private UserRole role;
+}
