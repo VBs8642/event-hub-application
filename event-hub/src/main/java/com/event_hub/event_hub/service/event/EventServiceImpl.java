99674bd7b748155ee85b2894eb84033a121b4bae
VBs8642 <vanesabombeva2@gmail.com>
2026-06-23 15:42:01 +0300

    feat: added EventServiceImpl, RegisterServiceImpl, UserServiceImpl.

---
+++ event-hub/src/main/java/com/event_hub/event_hub/service/event/EventServiceImpl.java
@@ -0,0 +1,8 @@
+package com.event_hub.event_hub.service.event;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventServiceImpl {
+
+}
---
+++ event-hub/src/main/java/com/event_hub/event_hub/service/registarion/RegistrationServiceImpl.java
@@ -0,0 +1,4 @@
+package com.event_hub.event_hub.service.registarion;
+
+public class RegistrationServiceImpl {
+}
