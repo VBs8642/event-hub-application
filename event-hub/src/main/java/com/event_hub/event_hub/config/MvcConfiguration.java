95539f26a369e4acb58284221eb55e1c74998378
VBs8642 <vanesabombeva2@gmail.com>
2026-07-11 23:31:27 +0300

    refactor MvcConfiguration.java, pom.xml, UserMapper.java

--- event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
+++ event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.event_hub.event_hub.config;
 
+import com.event_hub.event_hub.security.SessionInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
--- event-hub/src/main/java/com/event_hub/event_hub/mapper/user/UserMapper.java
+++ event-hub/src/main/java/com/event_hub/event_hub/mapper/user/UserMapper.java
@@ -1,4 +1,50 @@
 package com.event_hub.event_hub.mapper.user;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class UserMapper {
+    public class UserMapper {
+        if (userDto == null) {
+            return null;
+        }
+
+            return User.builder()
+                    .id(userDto.getId())
+                .username(userDto.getUsername())
+                .role(userDto.getRole())
+                .email(userDto.getEmail())
+                .build();
+    }
+
+    public static User toUserEntity(UserRegisterRequest userRegisterRequest) {
+        if (userRegisterRequest == null) {
+            return null;
+        }
+
+
+        return User.builder()
+                .username(userRegisterRequest.getUsername())
+                .password(userRegisterRequest.getPassword())
+                .email(userRegisterRequest.getEmail())
+                .firstName(userRegisterRequest.getFirstName())
+                .lastName(userRegisterRequest.getLastName())
+                .role(userRegisterRequest.getUserRole())
+                .build();
+    }
+
+    public static UserDto toUserDto(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .firstName(user.getFirstName()) // Safely maps user metrics
+                .lastName(user.getLastName())   // Safely maps user metrics
+                .role(user.getRole())
+                .build();
+    }
 }
