38f15308fe5c6c410077015706fe8ff7f712b434
VBs8642 <vanesabombeva2@gmail.com>
2026-07-12 00:12:24 +0300

    refactor: AgendaItemRepository.java

--- event-hub/src/main/java/com/event_hub/event_hub/repository/agendaItem/AgendaItemRepository.java
+++ event-hub/src/main/java/com/event_hub/event_hub/repository/agendaItem/AgendaItemRepository.java
@@ -1,10 +1,11 @@
 package com.event_hub.event_hub.repository.agendaItem;
 
 import com.event_hub.event_hub.model.entity.agendaItem.AgendaItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface AgendaItemRepository {
+public interface AgendaItemRepository extends JpaRepository<AgendaItem, UUID> {
     List<AgendaItem> findByEventIdOrderByDisplayOrderAscStartTimeAsc(UUID eventId);
 }
