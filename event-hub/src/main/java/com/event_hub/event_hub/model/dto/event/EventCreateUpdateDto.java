5528a422ea78c0e9c11e31c9f097dc1d8dd44f1a
VBs8642 <vanesabombeva2@gmail.com>
2026-07-12 23:48:42 +0300

    refactor: EventCreateUpdateDto, RegistrationDto

--- event-hub/src/main/java/com/event_hub/event_hub/model/dto/event/EventCreateUpdateDto.java
+++ event-hub/src/main/java/com/event_hub/event_hub/model/dto/event/EventCreateUpdateDto.java
@@ -1,10 +1,7 @@
 package com.event_hub.event_hub.model.dto.event;
 
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventCreateUpdateDto {
     @NotBlank(message = "Event title is required.")
     @Size(max = 100, message = "Title cannot exceed 100 characters.")
@@ -35,4 +33,6 @@ public class EventCreateUpdateDto {
     private LocalDateTime startDateTime;
     @NotNull(message = "End date and time are required.")
     private LocalDateTime endDateTime;
+
+
 }
--- event-hub/src/main/java/com/event_hub/event_hub/model/dto/registration/RegistrationDto.java
+++ event-hub/src/main/java/com/event_hub/event_hub/model/dto/registration/RegistrationDto.java
@@ -1,33 +1,46 @@
 package com.event_hub.event_hub.model.dto.registration;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Builder
 @Setter
 @Getter
 @AllArgsConstructor
 public class RegistrationDto {
-    @NotBlank(message = "Username is required.")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters.")
-    private String username;
+//    @NotBlank(message = "Username is required.")
+//    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters.")
+//    private String username;
+//
+//    @NotBlank(message = "Email is required.")
+//    @Email(message = "Please provide a valid email address.")
+//    private String email;
+//
+//    @NotBlank(message = "Password is required.")
+//    @Size(min = 6, message = "Password must be at least 6 characters long.")
+//    private String password;
+//
+//    @NotBlank(message = "First name is required.")
+//    private String firstName;
+//
+//    @NotBlank(message = "Last name is required.")
+//    private String lastName;
 
-    @NotBlank(message = "Email is required.")
-    @Email(message = "Please provide a valid email address.")
-    private String email;
+      @NotNull(message = "Target event parameter identifier context context context is required.")
+      private UUID eventId;
 
-    @NotBlank(message = "Password is required.")
-    @Size(min = 6, message = "Password must be at least 6 characters long.")
-    private String password;
+      @Min(value = 1, message = "You must reserve at least 1 seat pass.")
+      @Max(value = 5, message = "You cannot exceed buying 5 tickets per transaction pass line.")
+      private int attendeesCount;
 
-    @NotBlank(message = "First name is required.")
-    private String firstName;
+      private LocalDateTime registrationDate;
 
-    @NotBlank(message = "Last name is required.")
-    private String lastName;
+      private String status;
+
 }
