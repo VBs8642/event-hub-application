418e1738575243df72174c985facf465b1412c87
VBs8642 <vanesabombeva2@gmail.com>
2026-08-09 22:46:40 +0300

    feat: MvcConfiguration.java

--- event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
+++ event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
@@ -1,21 +1,55 @@
 package com.event_hub.event_hub.config;
 
 import com.event_hub.event_hub.security.SessionInterceptor;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
-    private final SessionInterceptor sessionInterceptor;
 
-    public MvcConfiguration(SessionInterceptor sessionInterceptor) {
-        this.sessionInterceptor = sessionInterceptor;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        http
+                .authorizeHttpRequests(matchers -> matchers
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .permitAll()
+                        .requestMatchers("/", "login", "register", "/error")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error")
+                        .permitAll()
+                ).logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                        .logoutSuccessUrl("/")
+                );
+
+
+        return http.build();
     }
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(sessionInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/images/**");
-    }
+
+
+//    private final SessionInterceptor sessionInterceptor;
+//
+//    public MvcConfiguration(SessionInterceptor sessionInterceptor) {
+//        this.sessionInterceptor = sessionInterceptor;
+//    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(sessionInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/css/**", "/images/**");
+//    }
+
+
 }
