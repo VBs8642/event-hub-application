431d3edd630cf3e76d19a6bdab764837f386fa9b
VBs8642 <vanesabombeva2@gmail.com>
2026-08-09 23:47:38 +0300

    refactor: MvcConfiguration.java

--- event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
+++ event-hub/src/main/java/com/event_hub/event_hub/config/MvcConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.util.AntPathMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
